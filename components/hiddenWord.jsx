7ee264a6eb8dfda4f596148bb22b1d552c9da6d0
matan77 <matan54828482@gmail.com>
2024-01-01 19:09:44 +0200

    feat: added incorrect guesses div

--- components/hiddenWord.jsx
+++ components/hiddenWord.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import wordObserver from "../utils/wordObserver";
-import { observer } from "mobx-react-lite";
 
 const HiddenWord = ({ }) => {
 
     return (
-        <div>
-            {`Guess the word: ${wordObserver.getWord}`}
-        </div>
+        <>
+            <div style={{ fontSize: 'x-large' }}>
+                {`Guess the word: ${wordObserver.getWord}`}
+
+            </div>
+            <br />
+            <div style={{
+                margin: 'auto',
+                border: '1px solid #000',
+                padding: '3px', width: 'fit-content', fontSize: 'large'
+            }}>
+                <h3>Incorrect Letters:</h3>
+                {wordObserver.incorrectGuesses.join(',')}
+            </div >
+        </>
     );
 };
 export default HiddenWord;
